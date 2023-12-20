327ab193ecb429dd663343bf06ff8171c9bab863
anshuljg07 <anshuljg07@gmail.com>
2023-12-20 11:54:37 -0600

    hello Pong, + Love2D init

---
+++ main.lua
@@ -0,0 +1,40 @@
+--[[
+    Pong Remake
+
+    Author: Anshul Gowda
+
+    Originally programmed by Atari in 1972. Features two
+    paddles, controlled by players, with the goal of getting
+    the ball past your opponent's edge. First to 10 points wins.
+
+    This version is built to more closely resemble the NES than
+    the original Pong machines or the Atari 2600 in terms of
+    resolution, though in widescreen (16:9) so it looks nicer on
+    modern systems.
+]]
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+--[[
+    Runs when the game first starts up, only once; used to initialize the game.
+]]
+function love.load()
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = false,
+        vsync = true
+    })
+end
+
+--[[
+    Called after update by LÖVE2D, used to draw anything to the screen, updated or otherwise.
+]]
+function love.draw()
+    love.graphics.printf(
+        'Hello Pong!',
+        0,
+        WINDOW_HEIGHT / 2 - 6,
+        WINDOW_WIDTH,
+        'center')
+end
